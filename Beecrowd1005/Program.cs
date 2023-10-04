24998b4c1b9e16ab768ab34d4efd52d730184207
PedroMoraes <pedroomoraes@gmail.com>
2023-10-04 14:48:51 -0300

    Programa finalizado e aceito pela plataforma Beecrowwd

--- Beecrowd1005/Program.cs
+++ Beecrowd1005/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd {
+    class Program1005 {
+        static void Main(string[] args)
+        {
+            double NotaA, NotaB, PesoA, PesoB, Media;
+
+            NotaA = double.Parse(Console.ReadLine());
+            NotaB = double.Parse(Console.ReadLine());
+
+            PesoA = 3.5;
+            PesoB = 7.5;
+
+            Media = ((NotaA * PesoA) + (NotaB * PesoB)) / (PesoA + PesoB);
+
+            Console.WriteLine("MEDIA = " +  Media.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
